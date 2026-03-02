7aaad91a0aedc095391124acb32d188fcdbc30a4
Rithvikraja <150163251+Rithvikraja@users.noreply.github.com>
2026-03-02 20:55:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -972,7 +972,7 @@ if "login" not in st.session_state:
 
 st.markdown("""
 <div class="card">
-<h1 style="color:green;">🏫 Smart Teacher Assistant</h1>
+<h1 style="color:green;">Smart Teacher Assistant</h1>
 <p style="text-align:center;font-size:18px;color:black;">
 AI + Digital Management System for Teachers
 </p>
@@ -999,6 +999,7 @@ if not st.session_state.login:
 
 else:
     dashboard()
+
 
 
 
