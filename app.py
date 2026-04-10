487ca59403bed88b906c900de0c926b126f9d9b3
Rithvikraja <150163251+Rithvikraja@users.noreply.github.com>
2026-04-10 15:09:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -668,7 +668,7 @@ def student_attendance():
         df.to_csv(ATT_FILE, index=False)
 
         st.success("✅ Attendance Marked Successfully")
-        st.balloons()
+        st.toast("✅ Attendance Marked", icon="🎉")
         # Reset session (important)
         
 
